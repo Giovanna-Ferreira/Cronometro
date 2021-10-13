e524245da0ed8482c5de4a454fe990e51fe2f38b
Giovanna-Ferreira <giovannamariadreger@gmail.com>
2021-10-13 19:43:26 -0300

    adicionando js

---
+++ Ações/script.js
@@ -0,0 +1,5 @@
+const startBtn = document.querySelector(".iniciar"),
+pareBtn = document.querySelector(".pare"),
+resetBtn = document.querySelector(".reset");
+
+startBtn.addEventListener("click", start)
