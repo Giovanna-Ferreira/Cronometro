643801124de6684be92e62b9e6016d1b1fa39b44
Giovanna-Ferreira <giovannamariadreger@gmail.com>
2021-10-14 14:34:08 -0300

    .

--- Ações/js/script.js
+++ Ações/js/script.js
@@ -1,20 +1,20 @@
 const startBtn = document.querySelector(".iniciar"),
-stopBtn = document.querySelector(".stop"),
-resetBtn = document.querySelector(".reset");
+stopBtn = document.querySelector(".parar"),
+resetBtn = document.querySelector(".reiniciar");
 
-startBtn.addEventListener("click", start);
-stopBtn.addEventListener("click", stop);
-resetBtn.addEventListener("click", reset);
+startBtn.addEventListener("click", iniciar);
+stopBtn.addEventListener("click", parar);
+resetBtn.addEventListener("click", reiniciar);
 
-function start() {
+function iniciar() {
     startBtn.classList.add("active");
     startBtn.classList.remove("stopActive");
 }
-function stop() {
+function parar() {
     startBtn.classList.remove("active");
     stopBtn.classList.add("stopActive");
 }
-function reset() {
+function reiniciar() {
     startBtn.classList.remove("active");
     stopBtn.classList.add("stopActive");
 }
