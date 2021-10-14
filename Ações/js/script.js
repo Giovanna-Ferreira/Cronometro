6939e1e89bb212e4c165bd3e0c88eb0539d60448
Giovanna-Ferreira <giovannamariadreger@gmail.com>
2021-10-14 17:33:08 -0300

    alterações

--- Ações/js/script.js
+++ Ações/js/script.js
@@ -1,5 +1,5 @@
-let hr = min = seg = ms = "0" + 0, 
- startTimer;
+let hr = min = sec = ms = "0" + 0,
+startTimer;
 
 const startBtn = document.querySelector(".iniciar"),
 stopBtn = document.querySelector(".parar"),
@@ -18,15 +18,15 @@ function iniciar() {
         ms = ms < 10 ? "0" + ms : ms;
 
         if(ms == 100){
-            seg++;
-            seg = seg < 10 ? "0" + seg : seg;
+            sec++;
+            sec = sec < 10 ? "0" + sec : sec;
             ms = "0" + 0;
             console.log(ms)
         }
-        if(seg == 60){
+        if(sec == 60){
             min++;
             min = min < 10 ? "0" + min : min;
-            seg = "0" + 0;
+            sec = "0" + 0;
         }
         if(min == 60){
             hr++;
@@ -35,6 +35,7 @@ function iniciar() {
         }
         
         putValue();
+
     },100); //1000ms = 1s
 
 }
@@ -42,13 +43,19 @@ function iniciar() {
 function parar() {
     startBtn.classList.remove("active");
     stopBtn.classList.add("stopActive");
+    clearInterval(startTimer);
 }
 function reiniciar() {
     startBtn.classList.remove("active");
     stopBtn.classList.remove("stopActive");
+    clearInterval(startTimer);
+    let hr = min = sec = ms = "0" + 0;
+    putValue();
 }
+
 function putValue() {
-    document.querySelector(".miliseg").innerHTML = ms;
-    document.querySelector(".seg").innerHTML = seg;
-    document.querySelector(".min").innerHTML = min;
+    document.querySelector(".milisecond").innerText = ms;
+    document.querySelector(".second").innerText = sec;
+    document.querySelector(".minute").innerText = min;
+    document.querySelector(".hour").innerText = hr;
 }
